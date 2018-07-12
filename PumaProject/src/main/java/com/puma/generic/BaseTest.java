0740371eb4afa0cd3d54ef4aefa5b2049872e9ef
Chaitra92 <33648270+Chaitra92@users.noreply.github.com>
2018-07-12 15:10:15 +0530

    extentreports

--- PumaProject/src/main/java/com/puma/generic/BaseTest.java
+++ PumaProject/src/main/java/com/puma/generic/BaseTest.java
@@ -5,13 +5,26 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Listeners;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+
+@Listeners(CustomListener.class)
 public class BaseTest implements AutoConstant
 {
 	public WebDriver driver;
+	ExtentReports extent;
+	 
 	//Opening Application
 	
+	@BeforeSuite
+	public void setUp(){
+		extent = new ExtentReports(System.getProperty("user.dir") +"\\reports\\er.html",true);
+	}
 	@BeforeMethod
 	public void openApp()
 	{
@@ -19,6 +32,7 @@ public class BaseTest implements AutoConstant
 		driver=new FirefoxDriver();
 		driver.get("https://in.puma.com/");
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		//driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 	}
 	
 	//Closing Application
@@ -28,4 +42,11 @@ public class BaseTest implements AutoConstant
 	{
 		driver.quit();
 	}
+	
+	@AfterSuite
+	public void tearDown(){
+		extent.flush();
+	    extent.close();
+	}
+	
 }
